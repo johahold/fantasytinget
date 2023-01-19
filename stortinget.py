987cf494b0dc6006e9b5f15b42c3e7389362b3de
frikkv <frikkvatneberg@gmail.com>
2023-01-19 13:37:35 +0100

    endring i politiker og stortinget

---
+++ stortinget.py
@@ -0,0 +1,9 @@
+from spillere import Spiller 
+class Stortinget: 
+    def __init__(self,spiller): 
+        self.tabell = []
+    def hent_spiller(self):
+        pass
+    def lag_tabell():
+        for players in spillere:
+            self.tabell.append(players)
