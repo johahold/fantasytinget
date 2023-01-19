ed98bf53123709c3707e85798184062a501fa408
frikkv <frikkvatneberg@gmail.com>
2023-01-19 13:57:57 +0100

    stortinget

--- stortinget.py
+++ stortinget.py
@@ -3,8 +3,8 @@ class Stortinget:
     def __init__(self): 
         self._tabell = []
     def hent_tabell(self):
-        self.tabell.sort()
-        x = [self.tabell[i].hent_navn() for i in range(len(self._tabell))]
+        x = [self._tabell[i].hent_poeng() for i in range(len(self._tabell))]
+        x.sort()
         return x
 
     def lag_tabell(self):
