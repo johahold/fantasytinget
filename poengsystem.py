3c530a249893ad07401498f1b08fc8f064f5c985
frikkv <73299308+frikkv@users.noreply.github.com>
2023-01-26 11:08:30 +0100

    lagd poengssystem og oppdatert poengsystem

--- poengsystem.py
+++ poengsystem.py
@@ -1,10 +1,19 @@
+from scraper import overskrifter
+from politiker import Politiker 
+
+
 class Poengsystem:
     def __init__(self):
         self._antall_ganger = 0
-        self._viktighet = 0
-    
-    def viktighet(self):
-        pass
+        self._poeng = 0  
 
     def antall_ganger(self):
         pass
+
+    def poeng(self):
+        for i in overskrifter:
+            viktighet = len(overskrifter)-i
+        for politiker in self._politiker:
+            if politiker in overskrifter:
+                self._poeng += viktighet
+                return self._poeng
--- politiker.py
+++ politiker.py
@@ -1,11 +1,14 @@
+from poengsystem import Poengsystem
+
 class Politiker:
     def __init__(self,pris,id,fylke):
         self._pris = pris 
         self._id = id 
         self._fylke = fylke
         self._poeng = 0 
-    def oppdater_poeng(): 
-        pass
+    def oppdater_poeng(self): 
+        Poengsystem.poeng(self)
+        self._poeng += Poengsystem.poeng() 
     
 
         
